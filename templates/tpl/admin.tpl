9337eaedcbdf8ce3980ca614d6a5119e0d3151a0
maxyeh666 <maxyeh666@gmail.com>
2020-02-05 13:47:38 +0800

    first commit

---
+++ templates/tpl/admin.tpl
@@ -0,0 +1,11 @@
+<h1 class = "text-center">管理員後台</h1>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-9">
+
+        </div>
+        <div class="col-sm-3">
+
+        </div>
+    </div>
+</div>
---
+++ templates_c/5fb75d60485f3dd38206eb56c5263d382a6c76e9_0.file.admin.tpl.php
@@ -0,0 +1,25 @@
+<?php
+/* Smarty version 3.1.34-dev-7, created on 2020-02-05 04:59:40
+  from 'D:\maxyeh\PHP\xampp\htdocs\web11\templates\tpl\admin.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.34-dev-7',
+  'unifunc' => 'content_5e3a3dac60c672_04337059',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '5fb75d60485f3dd38206eb56c5263d382a6c76e9' => 
+    array (
+      0 => 'D:\\maxyeh\\PHP\\xampp\\htdocs\\web11\\templates\\tpl\\admin.tpl',
+      1 => 1580875155,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5e3a3dac60c672_04337059 (Smarty_Internal_Template $_smarty_tpl) {
+?><h1>管理員登入成功!</h1><?php }
+}
